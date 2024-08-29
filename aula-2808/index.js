7b8aaa64a419fb12a85f80a0b378d60db8a715f0
Arthur Cavalcante <linsarthur02@gmail.com>
2024-08-28 21:48:08 -0300

    Add files via upload

---
+++ aula-2808/index.js
@@ -0,0 +1,86 @@
+// const frutas = ['Açaí', 'Banana', 'Caju', 'Damasco', 'Embauba', 'Figo']
+
+// const listaFrutas = frutas.map((fruta) => {
+//     return (console.log(fruta))
+// })
+
+//Questão 08
+// const palavra = prompt('Digite uma palavra: ').toUpperCase()
+// let contador = 0
+
+// const vogais = ["A", "E", "I", "O", "U"]
+// for(let i = 0; i < palavra.length; i++){
+//     if(vogais.includes(palavra[i])){
+//         contador++
+//     }
+    
+// }
+
+// console.log(contador)
+
+//questão 9
+
+// const palavra = prompt('Digite uma palavra: ')
+
+//     const arrayDeCaractere = palavra.split('')
+//     const arrayInvertido = arrayDeCaractere.reverse() 
+//     const stringInvertido = arrayInvertido.join('')
+//     console.log(stringInvertido)
+
+//questão 10
+
+// const numeros = [120, 12, 23, 43, 41, 55]
+// const maiorNumero = Math.max(...numeros)
+
+// console.log(maiorNumero)
+
+//questão 11
+
+// const numeros = [120, 12, 23, 43, 41, 55]
+// const menorNumero = Math.min(...numeros)
+
+// console.log(menorNumero)
+
+
+//questão 12 
+
+// const numeros = []
+// for(let i = 0; i <= 10; i ++){
+//     numeros.push(i);
+    
+// }
+// console.log(numeros)
+
+//questão 13
+// const numeros = [1, 2, 3, 4, 5]
+// 
+// for(let i = 0; i < numeros.length; i++){
+    
+//     console.log(numeros[i] * 2)
+
+// } 
+
+
+//questão 14
+// const numeros = [1, 2, 3, 4, 5]
+// let contador = 0
+// for(let i = 0; i < numeros.length; i++){
+//     contador += numeros[i]
+    
+// }
+// console.log(contador)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
